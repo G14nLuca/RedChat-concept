3bf4fb159c99b600f116c338ced02216ac0826aa
Gian Luca Oliveira <gloliveira539@gmail.com>
2024-02-03 17:25:51 -0300

    MenuManager is operational

--- src/menuManager.js
+++ src/menuManager.js
@@ -1,14 +1,79 @@
+const MENUS = require('./submenus/index.js');
+const menuInicial = require('./submenus/inicial.js')
+
 class MenuManager {
 
-  #manager
+  #client
+  #currentMenu
+  #welcomeMessage
 
-  constructor(manager) {
-    this.#manager = manager;
+  constructor(client) {
+    this.#client = client;
+    this.#currentMenu = MENUS.INICIAL;
+    this.#welcomeMessage = true;
   }
-  
-  getClient() {
-    if (this.#manager === undefined) throw new Error("Manager não iniciado");
-    return this.#manager;
+
+  setMenu(menu) {
+    this.#currentMenu = menu;
+  }
+
+  setWelcome(welcome) {
+    this.#welcomeMessage = welcome;
+  }
+
+  async handleReceivedMessage(message) {
+    console.log(`Mensagem recebida: ${message.body}`);
+    this.handleMessageMenu(message);
+  }
+
+  async handleMessageMenu(message) {
+    switch (this.#currentMenu) {
+
+      case MENUS.INICIAL:
+        if (this.#welcomeMessage) {
+          await menuInicial.handleMensagemBoasVindas(message, this);
+          this.setWelcome(false);
+        } else {
+          await menuInicial.handleMenuInicial(message, this);
+        }
+        break;
+
+     /* case MENUS.CATALOGO:
+        if (this.#welcomeMessage) {
+          await menuInicial.handleMensagemBoasVindas(message, this);
+          this.setWelcome(false);
+        } else {
+          await menuInicial.handleMenuInicial(message, this);
+        }
+        break;
+
+      case MENUS.CARRINHO:
+        if (this.#welcomeMessage) {
+          await menuInicial.handleMensagemBoasVindas(message, this);
+          this.setWelcome(false);
+        } else {
+          await menuInicial.handleMenuInicial(message, this);
+        }
+        break;
+
+      case MENUS.ATENDENTE:
+        if (this.#welcomeMessage) {
+          await menuInicial.handleMensagemBoasVindas(message, this);
+          this.setWelcome(false);
+        } else {
+          await menuInicial.handleMenuInicial(message, this);
+        }
+        break;*/
+    }
+  }
+
+  async sendMessage(destination, message) {
+    await this.#client.sendMessage(destination, message);
+  }
+
+  getManager() {
+    if (this === undefined) throw new Error("Manager não iniciado");
+    return this;
   }
 
 }
