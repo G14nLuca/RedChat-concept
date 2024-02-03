1194d6a0cb7a48e5acd59c50d48cdccdd9ed31e9
Gian Luca Oliveira <gloliveira539@gmail.com>
2024-02-03 17:25:13 -0300

    Support to sending of different messages

--- src/submenus/inicial.js
+++ src/submenus/inicial.js
@@ -1,27 +1,39 @@
-const messagens = [
-  {
-    titulo: "Bem-vindo",
-    conteudo: "Seja bem-vindo ao Clube do Controle, sua principal loja quando o assunto são games no geral. Por favor, selecione uma das opções abaixo: \n1 - Ver catálogo \n2 - Ver meu carrinho \n3 - Falar com atendente",
+const mensagens = {
+  bemVindo: {
+    content: "Seja bem-vindo ao Clube do Controle, sua principal loja quando o assunto são games no geral. Por favor, selecione uma das opções abaixo: \n1 - Ver catálogo \n2 - Ver meu carrinho \n3 - Falar com atendente",
   },
-  {
-    titulo: "Erro de escolha",
-    conteudo: "Por favor, escolha uma opção válida",
+  erroEscolha: {
+    content: "Por favor, escolha uma opção válida.",
   },
-  
-]
+};
+
+async function handleMensagemBoasVindas(message, menuManager) {
+  await menuManager.sendMessage(message.from, mensagens.bemVindo.content);
+}
 
 async function handleMenuInicial(message, menuManager) {
-  switch(message.body){
-    case "1":
-      menuManager.setMenu(1);
-      menuManager.ha
-  }
 
-  if (message.body != " ") {
-    await menuManager.sendMessage(message.from, welcomeMessage);
+  switch (message.body) {
+    case "1":
+      menuManager.setMenu(parseInt(message.body));
+      menuManager.handleMessageMenu(message);
+      break;
+    case "2":
+      menuManager.setMenu(parseInt(message.body));
+      menuManager.handleMessageMenu(message);
+      break;
+    case "3":
+      menuManager.setMenu(parseInt(message.body));
+      menuManager.handleMessageMenu(message);
+      break;
+    default:
+      await menuManager.sendMessage(message.from, mensagens.erroEscolha.content);
+      handleMensagemBoasVindas(message, menuManager);
+      break;
   }
 }
 
+
 module.exports = {
-  handleMenuInicial
+  handleMenuInicial, handleMensagemBoasVindas
 };
