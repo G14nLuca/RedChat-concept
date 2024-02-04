9771b2c9073d00af1021949f172782498a912343
Gian Luca Oliveira <gloliveira539@gmail.com>
2024-02-04 15:56:41 -0300

    Added catalog menu functionality

--- src/submenus/catalogo.js
+++ src/submenus/catalogo.js
@@ -1,21 +1,65 @@
+const items = require('./items_index');
 const mensagens = {
-  bemVindo: {
-    content: "Este é o menu do catálogo. Ainda em construção. Digite 0 para retornar ao menu principal.",
+  instrucoes: {
+    content: "Para adicionar um item ao carrinho, mande uma mensagem com a palavra 'add' e o número do id do item. \nPara avançar para a próxima página, digite 'próximo'. \nPara retroceder uma página, digite 'anterior'. \nPara ver seu carrinho, digite 'carrinho'. \nPara voltar ao menu principal, digite '0'. ",
   },
   erroEscolha: {
     content: "Por favor, escolha uma opção válida.",
   },
 };
 
+const itemsPerPage = 5;
+let currentPage = 1;
+
+async function sendCatalogPage(menuManager, page, destination) {
+  const startIdx = (page - 1) * itemsPerPage + 1;
+  const endIdx = startIdx + itemsPerPage - 1;
+
+  let catalogMessage = `Catálogo de itens - página ${page}:\n`;
+
+  for (let i = startIdx; i <= endIdx; i++) {
+    const item = items.get(i);
+    if (item) {
+      const [itemName, itemPrice] = item;
+      catalogMessage += `ID: ${i}, Nome: ${itemName}, Preço: R$${itemPrice}\n`;
+    }
+  }
+
+  await menuManager.sendMessage(destination, catalogMessage);
+}
+
 async function handleMenuCatalogo(message, menuManager) {
+
+  const destination = message.from;
+
   switch (message.body) {
+
+    case "próximo":
+      currentPage++;
+      await sendCatalogPage(menuManager, currentPage, destination);
+      break;
+
+    case "anterior":
+
+      if (currentPage == 1) {
+        await menuManager.sendMessage(destination, "Você já está na primeira página.");
+        await sendCatalogPage(menuManager, currentPage, destination);
+      } else {
+        currentPage--;
+        await sendCatalogPage(menuManager, currentPage, destination);
+      }
+
+      break;
+
     case "0":
       menuManager.setMenu(parseInt(message.body));
       menuManager.setWelcome(true);
       menuManager.handleMessageMenu(message);
       break;
+
     default:
-      await menuManager.sendMessage(message.from, mensagens.bemVindo.content);
+      await sendCatalogPage(menuManager, currentPage, destination);
+      await menuManager.sendMessage(destination, mensagens.instrucoes.content);
       break;
   }
 }
---
+++ src/submenus/items_index.js
@@ -0,0 +1,22 @@
+const items = new Map([
+    [1, ["Nintendo Switch", 1500]],
+    [2, ["The Legend Of Zelda - Tears of the Kingdom", 300]],
+    [3, ["Super Mario Bros. Wonder", 300]],
+    [4, ["PlayStation 5", 3500]],
+    [5, ["Marvel's Spider Man 2", 300]],
+    [6, ["The Last Of Us - Part I", 200]],
+    [7, ["Placa de vídeo Nvidia RTX 3050 8GB", 1500]],
+    [8, ["Teclado Mecânico sem fio RedDragon", 300]],
+    [9, ["Mouse sem Fio HyperX", 200]],
+    [10, ["Super Mario RPG", 300]],
+    [11, ["GTA V", 300]],
+    [12, ["EA FC 24 Edition", 200]],
+    [13, ["Nintendo Switch Lite", 900]],
+    [14, ["Princess Peach Showtime!", 300]],
+    [15, ["Just Dance 2024 Edition", 200]],
+  ]);
+  
+  module.exports = items;
+  
+
+module.exports = items;
